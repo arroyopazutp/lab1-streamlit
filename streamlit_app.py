947f93e1fa2a91d184e51b0d1050eb8a399f4110
arroyopazutp <133936789+arroyopazutp@users.noreply.github.com>
2023-07-03 09:49:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
 
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/books.csv")
+
+# Encabezado de la aplicación
+st.header("Visualización de datos - Libros")
+
+# Sección para filtrar los datos
+st.sidebar.header("Filtros")
+author = st.sidebar.selectbox("Autor", df["author"].unique())
+year = st.sidebar.slider("Año de publicación", 1900, 2022, 2020)
+price = st.sidebar.slider("Precio", 0.0, 100.0, (0.0, 100.0))
+
+# Filtrar los datos en base a los filtros establecidos
+filtered_df = df[(df["author"] == author) & (df["year"] == year) & (df["price"] >= price[0]) & (df["price"] <= price[1])]
+
+# Mostrar los resultados
+st.write(filtered_df)
