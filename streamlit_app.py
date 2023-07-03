866565ed0d89d89aba674b6b530663cdffe9d5c6
arroyopazutp <133936789+arroyopazutp@users.noreply.github.com>
2023-07-03 11:14:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,7 @@
+# import statements
 import streamlit as st
+import numpy as np
 import pandas as pd
-
-df = pd.read_csv("https://gist.github.com/jaidevd/23aef12e9bf56c618c41#file-books-csv")
-
-# Encabezado de la aplicación
-st.header("Visualización de datos - Libros")
-
-# Sección para filtrar los datos
-st.sidebar.header("Filtros")
-author = st.sidebar.selectbox("Autor", df["author"].unique())
-year = st.sidebar.slider("Año de publicación", 1900, 2022, 2020)
-price = st.sidebar.slider("Precio", 0.0, 100.0, (0.0, 100.0))
-
-# Filtrar los datos en base a los filtros establecidos
-filtered_df = df[(df["author"] == author) & (df["year"] == year) & (df["price"] >= price[0]) & (df["price"] <= price[1])]
-
-# Mostrar los resultados
-st.write(filtered_df)
+# adding title and text in the app
+st.title('Hello World')
+st.write("Congratulations! You have deployed your first web application.")
